fddcb63a05ffce81314d01520fe814306dd8c8b5
xubin <554531702@qq.com>
2018-08-06 15:17:22 +0800

    调整首页

--- src/main/java/com/renwu/backendapp/wallet/controller/AdminController.java
+++ src/main/java/com/renwu/backendapp/wallet/controller/AdminController.java
@@ -8,10 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 @Controller
